4c7b88ab09441507fcc208fd4360ddce2faab717
EduRamos95 <eduramos08.30@gmail.com>
2022-12-05 14:45:16 -0500

    fix: url users and key userId in todos

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -9,25 +9,27 @@ from sys import argv
 
 if __name__ == "__main__":
     # url base
-    url = 'https://jsonplaceholder.typicode.com/'
+    url_base = 'https://jsonplaceholder.typicode.com/'
     user_id = int(argv[1])
     EMPLOYEE_NAME = ""
     msg = 'Employee {} is done with tasks({}/{}):'
     # url (all_user, id_user, todos, cmp_task)
-    url_user = '{}user'.format(url)
+    url_user = '{}users'.format(url_base)
     url_id = '{}{}'.format(url_user, user_id)
-    url_todos = '{}/todos'.format(url_id)
+    url_todos = '{}todos'.format(url_base)
     url_cmp_tk = '{}?completed=true'.format(url_todos)
-    response_user = request.get(url_user)
-    user_json = responde_user.json()
+    response_user = requests.get(url_user)
+    # print("--- response_user: {} ".format(response_user))
+    user_json = response_user.json()
+    # print(type(user_json),"-->",user_json)
     for i in user_json:
         if i.get("id") == user_id:
             EMPLOYEE_NAME = i.get("name")
-    response_todos = requets.get(url_todos)
+    response_todos = requests.get(url_todos)
     todos_json = response_todos.json()
     DONE_TASKS = []
     for i in todos_json:
-        if i.get("userId") == user_id and i.get("completed" is True):
+        if i.get("userId") == user_id and i.get("completed") is True:
             DONE_TASKS.append(i.get("title"))
     NUMBER_OF_DONE_TASKS = len(DONE_TASKS)
     TOTAL_NUMBER_OF_TASK = 0
@@ -37,7 +39,7 @@ if __name__ == "__main__":
             TOTAL_NUMBER_OF_TASK += 1
     print("Employee {} is done with tasks({}/{}):"
           .format(EMPLOYEE_NAME,
-                  NUMBER_OF_DONE_TASK,
+                  NUMBER_OF_DONE_TASKS,
                   TOTAL_NUMBER_OF_TASK))
     for i in DONE_TASKS:
         print("\t {}".format(i))
